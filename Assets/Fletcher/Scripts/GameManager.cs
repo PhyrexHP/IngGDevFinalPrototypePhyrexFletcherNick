7e8330fe96c4cdfd90f29c8d75f1932388219444
XiRdnethFr <yg2540@nyu.edu>
2022-11-07 06:52:32 +0800

    Fletcher Commit_2

--- Assets/Fletcher/Scripts/GameManager.cs
+++ Assets/Fletcher/Scripts/GameManager.cs
@@ -8,16 +8,26 @@ public class GameManager : MonoBehaviour
     public GameObject head;
     int headChoiceIndex = 0;
 
+    public GameObject body;
+    int bodyChoiceIndex = 0;
+
     public TMP_Text headBox;
+    public TMP_Text bodyBox;
 
     public List<string> headChoicesNumber;
     public List<Sprite> headChoicesSprite;
 
+    public List<string> bodyChoicesNumber;
+    public List<Sprite> bodyChoicesSprite;
+
     private SpriteRenderer headSR;
+    private SpriteRenderer bodySR;
     // Start is called before the first frame update
     void Start()
     {
         headSR = head.GetComponent<SpriteRenderer>();
+        bodySR = body.GetComponent<SpriteRenderer>();
+        setBody();
         setHead();
     }
 
@@ -39,9 +49,33 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void nextBodyChoice()
+    {
+        if (bodyChoiceIndex < (bodyChoicesNumber.Count - 1))
+        {
+            bodyChoiceIndex++;
+            setBody();
+        }
+    }
+
+    public void prevBodyChoice()
+    {
+        if (bodyChoiceIndex > 0)
+        {
+            bodyChoiceIndex--;
+            setBody();
+        }
+    }
+
     void setHead()
     {
         headSR.sprite = headChoicesSprite[headChoiceIndex];
         headBox.text = headChoicesNumber[headChoiceIndex];
+    }
+
+    void setBody()
+    {
+        bodySR.sprite = bodyChoicesSprite[bodyChoiceIndex];
+        bodyBox.text = bodyChoicesNumber[bodyChoiceIndex];
     }
 }
